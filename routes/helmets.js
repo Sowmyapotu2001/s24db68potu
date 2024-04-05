7277e2bf5a78a4c79efd20cc8a74ef33981880a9
Sowmyapotu2001 <potusowmya2001@gmail.com>
2024-04-05 10:56:32 -0500

    added all

--- routes/helmets.js
+++ routes/helmets.js
@@ -1,10 +1,13 @@
 var express = require('express');
+const helmets_controller= require('../controllers/helmets');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('helmets', { title: 'Search Results: Helmets' });
-});
-
-
+// router.get('/', function(req, res, next) {
+//   res.render('helmets', { title: 'Search Results: Helmets' });
+// });
+router.get('/', helmets_controller.helmets_view_all_Page );
 module.exports = router;
+
+
+
