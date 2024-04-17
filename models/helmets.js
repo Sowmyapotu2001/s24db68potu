306a7d3ab6c0028ba831e3bbff01df7156cb74ab
Sowmyapotu2001 <potusowmya2001@gmail.com>
2024-04-16 22:41:07 -0500

    added all

--- models/helmets.js
+++ models/helmets.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 const helmetsSchema = mongoose.Schema({
-helmet_style: String,
+helmet_style: {
+    type: String,
+    minlength:3,
+    maxlength:15,
+},
 size: String,
-price: Number
+price: 
+{
+    type:Number,
+    min:2,
+    max:8
+},
 })
 module.exports = mongoose.model("helmets",
 helmetsSchema)
--- routes/helmets.js
+++ routes/helmets.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const helmets_controller= require('../controllers/helmets');
 var router = express.Router();
+var passport=require('passport');
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -16,15 +23,18 @@ router.get('/detail', helmets_controller.helmets_view_one_Page);
 router.get('/create', helmets_controller.helmets_create_Page);
 
 /* GET create update page */
-router.get('/update', helmets_controller.helmets_update_Page);
+router.get('/update',secured, helmets_controller.helmets_update_Page);
 
 /* GET delete Bank page */
 router.get('/delete', helmets_controller.helmets_delete_Page);
 
 
-router.get('/update',helmets_controller.helmets_update_Page);
+// router.get('/update',helmets_controller.helmets_update_Page);
 
-
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+    
 
 module.exports = router;
 
